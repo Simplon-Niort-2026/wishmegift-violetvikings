e83c2dcd8550edbe054d9300721ab2f74d37fe83
Bastien <29610890+bastienapp@users.noreply.github.com>
2026-04-22 13:41:52 +0200

    Initial commit

---
+++ src/main/java/co/simplon/wishmegift/WishmegiftApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.wishmegift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishmegiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishmegiftApplication.class, args);
+	}
+
+}

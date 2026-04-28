e305d1837ebd71ec8133fc931eb4a921203210ee
POUZET Theo <theo.pouzet@maif.fr>
2026-04-28 14:12:38 +0200

    featurs crud wishList, partage via email non crée

--- src/main/java/co/simplon/wishmegift/controller/WishlistController.java
+++ src/main/java/co/simplon/wishmegift/controller/WishlistController.java
@@ -8,7 +8,14 @@ import co.simplon.wishmegift.repository.WishlistRepository;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -26,5 +33,30 @@ public class WishlistController {
         public List<Wishlist> getAllWishLists(){
             return repository.findAll();
         } 
+        @PostMapping
+        public Wishlist createWishList(@RequestBody Wishlist newWishlist) {
+            
+            return repository.save(newWishlist);
+        }
         
-}
+        @DeleteMapping("/{id}")
+            public void deleteWishList(@PathVariable Long id){
+                repository.deleteById(id);
+            }
+        
+        @PutMapping("/{id}")
+        public Wishlist updateWishList(@PathVariable Long id, @RequestBody Wishlist updateWishList) {
+            return repository.findById(id)
+            .map(WishList ->{
+                WishList.setNom(updateWishList.getNom());
+                WishList.setDescription(updateWishList.getDescription());
+                WishList.setTheme(updateWishList.getTheme());
+                WishList.setAuteur(updateWishList.getAuteur());
+                return repository.save(WishList);
+            })
+            .orElseGet(() ->{
+                updateWishList.setId(id);
+                return repository.save(updateWishList);
+            });
+        }
+        }
